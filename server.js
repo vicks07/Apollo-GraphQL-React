dea75b9337d16862aac1225873628651ee55c044
vicks07 <vikram.january@gmail.com>
2019-10-29 14:59:44 +0530

    GraphQL server with React

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const graphqlHttp = require('express-graphql');
 const schema = require('./schema.js');
-
+const cors = require('cors');
 const app = express();
+const path = require('path');
 
-
+// Allow Cross-Origin
+app.use(cors());
 
 const port = process.env.PORT || 4000;
 
@@ -12,6 +14,13 @@ app.use('/graphql', graphqlHttp({
     schema,
     graphiql: true
 }))
+
+
+app.use(express.static('public'));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'public','index.html'));
+})
  
 app.listen(port ,()=>{
     console.log(`Server started on ${port}`);
